9497a6228ce91ec42293d6197eaa2ecf908e4c60
seitamuro <seitamuro0210@gmail.com>
2023-10-23 10:48:55 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'p5js-in-ionic',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
