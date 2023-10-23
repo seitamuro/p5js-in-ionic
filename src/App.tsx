53bdd432c374266919d4abfcbf63053d0e8d9daf
seitamuro <seitamuro0210@gmail.com>
2023-10-23 10:51:01 +0900

    初期セットアップ完了

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,10 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+defineCustomElements(window);
+
 setupIonicReact();
 
 const App: React.FC = () => (
